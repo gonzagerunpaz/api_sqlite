17079de3526cd4d004e5b86773f5ae3bdc8963ca
Gonzalez <B060018@servexternos.santandertecnologia.com.ar>
2025-04-16 21:08:52 -0300

    Seguimos con db

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const db = require("./db/models");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// Esta linea pone por default recibir json en el body
+app.use(express.json());
+
 app.get("/user", async (req, res) => {
   const data = await db.User.findAll({});
   res.status(200).json(data);
@@ -13,6 +16,23 @@ app.get("/user/:id", async (req, res) => {
   const data = await db.User.findByPk(id);
   if (data) res.status(200).json(data);
   else res.status(404).json({ mensaje: `El ${id} no se encuentra` });
+});
+
+app.post("/user", async (req, res) => {
+  const body = req.body;
+  const user = await db.User.create(body);
+  res.status(201).json(user);
+});
+
+app.delete("/user/:id", async (req, res) => {
+  const id = req.params.id;
+  const data = await db.User.findByPk(id);
+  if (data) {
+    await data.destroy();
+    res
+      .status(200)
+      .json({ mensaje: `El ${id} se elimino correctamente`, user: data });
+  } else res.status(404).json({ mensaje: `El ${id} no se encuentra` });
 });
 
 app.listen(PORT, () => {
